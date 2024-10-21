b6ae6011a49932eb9988918033467adf88ee9671
Jefferson Alquinga <144504631+Jeffers199817@users.noreply.github.com>
2024-10-21 11:21:01 -0500

    Inicio del proyecto Sistema de roles y permisos

---
+++ src/main/java/com/mycompany/sistemaderolesypermisos/SistemaDeRolesyPermisos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemaderolesypermisos;
+
+/**
+ *
+ * @author JEFFERSON ALQUINGA
+ */
+public class SistemaDeRolesyPermisos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
